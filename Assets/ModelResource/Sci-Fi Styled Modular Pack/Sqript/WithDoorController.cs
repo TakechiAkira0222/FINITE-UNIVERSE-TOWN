db2d7f3f839265ef304fe774b01687614b00e7ca
TakechiAkira0222 <99384193+TakechiAkira0222@users.noreply.github.com>
2022-11-27 00:55:21 +0900

    2022/11/27 Map UpDate
    Map Color Chege

--- Assets/ModelResource/Sci-Fi Styled Modular Pack/Sqript/WithDoorController.cs
+++ Assets/ModelResource/Sci-Fi Styled Modular Pack/Sqript/WithDoorController.cs
@@ -6,18 +6,38 @@ namespace Takechi.MapObject.AnimatorController
 {
     public class WithDoorController : MonoBehaviour
     {
+        #region SerialzeField
         [SerializeField] private Animator m_animator;
+        [SerializeField] private bool     m_isOperation = true; 
+        [SerializeField] private float    m_openingTime = 3;
+        #endregion
 
+        #region UnityEvent
         void Reset()
         {
+            m_isOperation = true;
+            m_openingTime = 3;
             m_animator = this.transform.parent.gameObject.GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            // ‰¼
+            if(Vector3.Distance(this.gameObject.transform.position, Camera.main.transform.position) <= 2)
+            {
+                m_animator.SetBool("character_nearby", true);
+
+                Invoke(nameof(OnCharacter_nearbyFalse), m_openingTime);
+            }
         }
 
         void OnCollisionEnter(Collision collision)
         {
+            if (!m_isOperation) return;
+
             m_animator.SetBool("character_nearby", true);
 
-            Invoke(nameof(OnCharacter_nearbyFalse), 3);
+            Invoke(nameof(OnCharacter_nearbyFalse), m_openingTime);
         }
 
         void OnCollisionExit(Collision collision)
@@ -29,5 +49,7 @@ namespace Takechi.MapObject.AnimatorController
         {
             m_animator.SetBool("character_nearby", false);
         }
+        #endregion
+
     }
 }
