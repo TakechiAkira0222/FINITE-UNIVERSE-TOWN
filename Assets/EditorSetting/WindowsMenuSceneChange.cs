b9e568a15a2315a6196e5d20eac5c80ce247b874
TakechiAkira0222 <99384193+TakechiAkira0222@users.noreply.github.com>
2022-12-03 23:37:36 +0900

    2022/12/03 training map input

--- Assets/EditorSetting/WindowsMenuSceneChange.cs
+++ Assets/EditorSetting/WindowsMenuSceneChange.cs
@@ -15,26 +15,33 @@ namespace Takechi.WindowsMenu.SceneChange
 
         private static void SceneSwitchingScene00()
         {
-            EditorSceneManager.OpenScene(ScenesFolderPas + "TitleScnene.unity", OpenSceneMode.Single);
+            EditorSceneManager.OpenScene(ScenesFolderPas + "/TitleScneneFolder/" + "TitleScnene.unity", OpenSceneMode.Single);
         }
 
         [MenuItem("SceneSwitching / LobbyScene _#F2")]
         private static void SceneSwitchingScene01()
         {
-            EditorSceneManager.OpenScene(ScenesFolderPas + "LobbyScene.unity", OpenSceneMode.Single);
+            EditorSceneManager.OpenScene(ScenesFolderPas + "/LobbySceneFolder/" + "LobbyScene.unity", OpenSceneMode.Single);
         }
 
         [MenuItem("SceneSwitching / CharacterSelectionScene _#F3")]
         private static void SceneSwitchingScene02()
         {
-            EditorSceneManager.OpenScene(ScenesFolderPas + "CharacterSelectionScene.unity", OpenSceneMode.Single);
+            EditorSceneManager.OpenScene(ScenesFolderPas + "/CharacterSelectionSceneFolder/" + "CharacterSelectionScene.unity", OpenSceneMode.Single);
         }
 
         [MenuItem("SceneSwitching / FutureCityScene _#F4")]
         private static void SceneSwitchingScene03()
         {
-            EditorSceneManager.OpenScene(ScenesFolderPas + "FutureCityScene.unity", OpenSceneMode.Single);
+            EditorSceneManager.OpenScene(ScenesFolderPas + "/FutureCitySceneFolder/" + "FutureCityScene.unity", OpenSceneMode.Single);
         }
+
+        [MenuItem("SceneSwitching / TrainingGroundScene _#F5")]
+        private static void SceneSwitchingScene04()
+        {
+            EditorSceneManager.OpenScene(ScenesFolderPas + "/TrainingGroundSceneFolder/" + "TrainingGroundScene.unity", OpenSceneMode.Single);
+        }
+        
     }
 }
 #endif
--- Assets/TakechiAsset/UiControllerSystem/ManagementButtonSystem/ButtonSceneStateChangeManagement.cs
+++ Assets/TakechiAsset/UiControllerSystem/ManagementButtonSystem/ButtonSceneStateChangeManagement.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class ButtonSceneStateChangeManagement : MonoBehaviour
+namespace Takechi.ManagmentButton.SrateChange
 {
-    void Start()
+    public class ButtonSceneStateChangeManagement : MonoBehaviour
     {
-        
-    }
+        private void Reset()
+        {
+        }
 
-    void Update()
-    {
-        
+        public void OnSceneChange(int number)
+        {
+            SceneManager.LoadScene(number);
+            Debug.Log($"<color=yellow> SceneChange </color>({number})");
+        }
+
+        public void OnSceneChange(string name)
+        {
+            SceneManager.LoadScene(name);
+            Debug.Log($"<color=yellow> SceneChange </color>({name})");
+        }
     }
 }
