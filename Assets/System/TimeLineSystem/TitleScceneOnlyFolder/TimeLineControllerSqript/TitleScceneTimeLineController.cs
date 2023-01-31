8e85937de06cdf82de0affe937c2f99f645ef8af
TakechiAkira0222 <99384193+TakechiAkira0222@users.noreply.github.com>
2023-01-31 12:01:00 +0900

    2023/01/31 title update

--- Assets/System/TimeLineSystem/TitleScceneOnlyFolder/TimeLineControllerSqript/TitleScceneTimeLineController.cs
+++ Assets/System/TimeLineSystem/TitleScceneOnlyFolder/TimeLineControllerSqript/TitleScceneTimeLineController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.RestService;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
@@ -10,6 +11,14 @@ public class TitleScceneTimeLineController : MonoBehaviour
 {
     #region SerializeField
 
+    [SerializeField] PlayableDirector m_playableDirector;
+
+    #endregion
+
+    #region private variable
+
+    PlayableDirector playableDirector => m_playableDirector;
+
     #endregion
     private void Director_Played(PlayableDirector obj)
     {
@@ -18,16 +27,28 @@ public class TitleScceneTimeLineController : MonoBehaviour
 
     private void Director_Stopped(PlayableDirector obj)
     {
-        
+        SceneManager.LoadScene(SceneName.lobbyScene);
     }
 
     private void Reset()
     {
-       
+        m_playableDirector = this.gameObject.GetComponent<PlayableDirector>();
+    }
+
+    private void OnEnable()
+    {
+        playableDirector.paused  += Director_Played;
+        playableDirector.stopped += Director_Stopped;
+    }
+
+    private void OnDisable()
+    {
+        playableDirector.paused  -= Director_Played;
+        playableDirector.stopped -= Director_Stopped;
     }
 
     public void OnGamePlayStart()
     {
-        SceneManager.LoadScene(SceneName.lobbyScene);
+        playableDirector.Play();
     }
 }
